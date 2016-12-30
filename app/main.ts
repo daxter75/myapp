5e205cb34a19697a692f3203826a3d7b55ac4717
daxter75 <daxter75@gmail.com>
2016-12-30 14:44:28 +0100

    first

---
+++ app/main.ts
@@ -0,0 +1,11 @@
+/*
+https://www.youtube.com/watch?v=-zW1zHqsdyc
+http://jsonplaceholder.typicode.com/posts
+ */
+
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
